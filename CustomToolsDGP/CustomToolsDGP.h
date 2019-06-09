1284c3fe862c9aecb718a021b67f5dcfd69d3904
David Galán <david.galan@studiogenesis.es>
2019-06-09 14:31:24 +0200

    first commit

---
+++ CustomToolsDGP/CustomToolsDGP.h
@@ -0,0 +1,19 @@
+//
+//  CustomToolsDGP.h
+//  CustomToolsDGP
+//
+//  Created by David Galán on 09/06/2019.
+//  Copyright © 2019 David Galán. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomToolsDGP.
+FOUNDATION_EXPORT double CustomToolsDGPVersionNumber;
+
+//! Project version string for CustomToolsDGP.
+FOUNDATION_EXPORT const unsigned char CustomToolsDGPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomToolsDGP/PublicHeader.h>
+
+
---
+++ CustomToolsDGP/Files/TestFIle.swift
@@ -0,0 +1,14 @@
+//
+//  TestFIle.swift
+//  CustomToolsDGP
+//
+//  Created by David Galán on 09/06/2019.
+//  Copyright © 2019 David Galán. All rights reserved.
+//
+
+import Foundation
+
+
+public func printText() {
+    print("print text sampel ok")
+}
