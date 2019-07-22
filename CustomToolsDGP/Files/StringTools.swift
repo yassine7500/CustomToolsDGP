b69dfb7edf2cbfe820a9645ca1c7de1bbe5b044f
David Galán <david.galan@studiogenesis.es>
2019-07-22 12:16:53 +0200

    Updated StringTools file.

--- CustomToolsDGP/Files/StringTools.swift
+++ CustomToolsDGP/Files/StringTools.swift
@@ -8,7 +8,7 @@
 
 extension String {
     
-    var isNumeric: Bool {
+    public var isNumeric: Bool {
         
         if Int(self) != nil || Double(self) != nil || Float(self) != nil {
             return true
@@ -21,8 +21,23 @@ extension String {
         //        return Set(self.characters).isSubset(of: nums)
     }
     
-    var toLocale: Locale {
+    public var toLocale: Locale {
         return Locale(identifier: self)
+    }
+    
+    public func capitalizingFirstLetter() -> String {
+        return prefix(1).uppercased() + self.lowercased().dropFirst()
+    }
+    
+    public mutating func capitalizeFirstLetter() {
+        self = self.capitalizingFirstLetter()
+    }
+    
+    public func underlineText() ->  NSMutableAttributedString {
+        let textRange = NSMakeRange(0, self.count)
+        let attributedText = NSMutableAttributedString(string: self)
+        attributedText.addAttribute(NSAttributedString.Key.underlineStyle , value: NSUnderlineStyle.single.rawValue, range: textRange)
+        return attributedText
     }
     
 }
@@ -41,6 +56,71 @@ public class StringTools {
         let components = text.components(separatedBy: .whitespacesAndNewlines)
         let words = components.filter { !$0.isEmpty }
         return words.count
+    }
+    
+    public func setCustomText(text: String, size: CGFloat = 14, weight: UIFont.Weight = .medium, color: UIColor? = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), jumpSpace: Bool = false) -> NSAttributedString {
+        
+        let labelFont = UIFont.systemFont(ofSize: size, weight: weight)
+        var attributes: [NSAttributedString.Key : Any]!
+        
+        if color != nil {
+            attributes = [NSAttributedString.Key.font : labelFont, NSAttributedString.Key.foregroundColor : color!]
+        } else {
+            attributes = [NSAttributedString.Key.font : labelFont]
+        }
+        
+        let textAattr = NSAttributedString(string: text, attributes: attributes as [NSAttributedString.Key : Any])
+        
+        let result = NSMutableAttributedString()
+        result.append(textAattr)
+        
+        if jumpSpace {
+            let textSpace = NSAttributedString(string: "\n")
+            result.append(textSpace)
+        }
+        
+        if color != nil {
+            
+        }
+        
+        return result
+    }
+    
+    public func setCustomTwoTexts(textA: String, textB: String, sizeA: CGFloat = 20, sizeB: CGFloat = 20, weightA: UIFont.Weight = .medium, weightB: UIFont.Weight = .medium, colorA: UIColor = #colorLiteral(red: 0.4, green: 0.4, blue: 0.4, alpha: 1), colorB: UIColor = #colorLiteral(red: 0.4, green: 0.4, blue: 0.4, alpha: 1), jumpSpace: Bool = false) -> NSAttributedString {
+        
+        var labelFont = UIFont.systemFont(ofSize: sizeA, weight: weightA)
+        var attributes: Dictionary = [NSAttributedString.Key.font : labelFont, NSAttributedString.Key.foregroundColor : colorA]
+        let textAattr = NSAttributedString(string: textA, attributes: attributes as [NSAttributedString.Key : Any])
+        
+        labelFont = UIFont.systemFont(ofSize: sizeB, weight: weightB)
+        attributes = [NSAttributedString.Key.font : labelFont, NSAttributedString.Key.foregroundColor : colorB]
+        let textBattr = NSAttributedString(string: textB, attributes: attributes as [NSAttributedString.Key : Any])
+        
+        let result = NSMutableAttributedString()
+        result.append(textAattr)
+        
+        if jumpSpace {
+            let textSpace = NSAttributedString(string: "\n")
+            result.append(textSpace)
+        }
+        result.append(textBattr)
+        
+        return result
+    }
+    
+    
+    public func setCustomEmailTextBetweenTexts(textA: String, email: String, textB: String) -> NSMutableAttributedString {
+        
+        let messageText = NSMutableAttributedString()
+        let textA = setCustomText(text: textA, size: 20, weight: .regular, color: nil, jumpSpace: true)
+        let textEmail = setCustomText(text: email, size: 20, weight: .bold, color: nil, jumpSpace: true)
+        let textB = setCustomText(text: textB, size: 20, weight: .regular, color: nil, jumpSpace: false)
+        
+        messageText.append(textA)
+        messageText.append(textEmail)
+        messageText.append(textB)
+        
+        return messageText
     }
     
 }
