aa7ce6f81b94c0a0f569e9a5d636365eccbcaa53
David Galán <david.galan@studiogenesis.es>
2019-07-22 15:13:10 +0200

    Added EmailTools file.

---
+++ CustomToolsDGP/Files/EmailTools.swift
@@ -0,0 +1,25 @@
+//
+//  EmailTools.swift
+//  CustomToolsDGP
+//
+//  Created by David Galán on 22/07/2019.
+//  Copyright © 2019 David Galán. All rights reserved.
+//
+
+extension String {
+    
+    func isValidEmail() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: self)
+    }
+    
+    func openEmail() {
+        if self.isValidEmail() {
+            if let url = URL(string: "mailto:\(self)") {
+                UIApplication.shared.open(url)
+            }
+        }
+    }
+    
+}
