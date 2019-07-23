c0eab21b6c9db44e57646f0002d64c94859510c7
David Galán <david.galan@studiogenesis.es>
2019-07-23 16:46:00 +0200

    Updated encryption method.

--- CustomToolsDGP/Files/Md5hashTool.swift
+++ CustomToolsDGP/Files/Md5hashTool.swift
@@ -6,6 +6,10 @@
 //  Copyright © 2019 David Galán. All rights reserved.
 //
 
+import Foundation
+import var CommonCrypto.CC_MD5_DIGEST_LENGTH
+import func CommonCrypto.CC_MD5
+import typealias CommonCrypto.CC_LONG
 
 public class Md5hashTool {
     
@@ -14,15 +18,19 @@ public class Md5hashTool {
     }
     
     public func MD5(string: String) -> Data {
+        let length = Int(CC_MD5_DIGEST_LENGTH)
         let messageData = string.data(using:.utf8)!
-        var digestData = Data(count: Int(CC_MD5_DIGEST_LENGTH))
+        var digestData = Data(count: length)
         
-        _ = digestData.withUnsafeMutableBytes {digestBytes in
-            messageData.withUnsafeBytes {messageBytes in
-                CC_MD5(messageBytes, CC_LONG(messageData.count), digestBytes)
+        _ = digestData.withUnsafeMutableBytes { digestBytes -> UInt8 in
+            messageData.withUnsafeBytes { messageBytes -> UInt8 in
+                if let messageBytesBaseAddress = messageBytes.baseAddress, let digestBytesBlindMemory = digestBytes.bindMemory(to: UInt8.self).baseAddress {
+                    let messageLength = CC_LONG(messageData.count)
+                    CC_MD5(messageBytesBaseAddress, messageLength, digestBytesBlindMemory)
+                }
+                return 0
             }
         }
-        
         return digestData
     }
     
