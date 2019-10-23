cf07dd0d3c4cb3d3a7ff49123c94c9fda58e203e
David Galán <david.galan@studiogenesis.es>
2019-10-23 13:22:25 +0200

    Updated annotations.

--- CustomToolsDGP/Files/PrintTools.swift
+++ CustomToolsDGP/Files/PrintTools.swift
@@ -7,38 +7,66 @@
 //
 
 public enum PrintType: String {
+    
+    /// 📘
     case blue = "📘"
+    /// 📗
     case green = "📗"
+    /// 📙
     case orange = "📙"
+    /// 📕
     case red = "📕"
     
+    /// ⚠️
     case warning = "⚠️"
+    /// ✅
     case success = "✅"
+    /// ❌
     case error = "❌"
     
+    /// ❗️
     case exclamationRed = "❗️"
+    /// ❕
     case exclamationWhite = "❕"
+    /// ❓
     case questionRed = "❓"
+    /// ❔
     case quesationWhite = "❔"
+    /// ‼️
     case exclamationDouble = "‼️"
+    /// ⁉️
     case exclamationQuestion = "⁉️"
     
+    /// 👍
     case ok = "👍"
+    /// 👎
     case nok = "👎"
     
+    /// ⏱
     case time = "⏱"
+    /// 📅
     case calendar = "📅"
+    /// 📌
     case pin = "📌"
     
+    /// 🔒
     case lockClose = "🔒"
+    /// 🔓
     case lockOpen = "🔓"
     
+    /// 🅿️
     case parking = "🅿️"
+    /// 🏠
     case address = "🏠"
+    /// ☎️
     case phone = "☎️"
+    /// ✉️
     case email = "✉️"
+    /// 💰
     case money = "💰"
+    /// 📎
     case clip = "📎"
+    /// 🔑
     case token = "🔑"
 }
 
