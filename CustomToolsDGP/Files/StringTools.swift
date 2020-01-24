c4dc876fecb2398580f6507b14632dd7a2376035
David Galán <david.galan@studiogenesis.es>
2020-01-24 10:57:21 +0100

    Added containsSpecialCharacter check in StringTools.

--- CustomToolsDGP/Files/StringTools.swift
+++ CustomToolsDGP/Files/StringTools.swift
@@ -54,6 +54,12 @@ extension String {
         return passowrdTest.evaluate(with: self)
     }
     
+    public var containsSpecialCharacter: Bool {
+       let regex = ".*[^A-Za-z0-9].*"
+       let testString = NSPredicate(format:"SELF MATCHES %@", regex)
+       return testString.evaluate(with: self)
+    }
+    
 }
 
 public class StringTools {
