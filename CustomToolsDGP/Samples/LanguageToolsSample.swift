3bc9fd23bf34d4dd02d9ba84ee1fea5f8f141d09
David Galán <david.galan@studiogenesis.es>
2020-02-06 16:11:21 +0100

    Updated samples.

--- CustomToolsDGP/Samples/LanguageToolsSample.swift
+++ CustomToolsDGP/Samples/LanguageToolsSample.swift
@@ -5,155 +5,200 @@
 //  Created by David Galán on 21/07/2019.
 //  Copyright © 2019 David Galán. All rights reserved.
 //
-
-//// MARK: LANGUAGES CODES
-//public enum LanguageCodeType: String {
-//    case cat = "ca" //optional: "ca-ES"
-//    case cas = "es"
-//    case eng = "en"
-//}
 //
-//// MARK: VARIABLES
-//var bundleLanguage = Bundle()
-//public let appLanguages = [LanguageCodeType.cat, LanguageCodeType.cas, LanguageCodeType.eng]
-//
-//// MARK: DEFAULTS REFERENCES
-//
-//public var languageCodeEdited: Bool {
-//    set {
-//        UserDefaults.standard.set(newValue, forKey: "languageCodeEdited")
-//    }
-//    get {
-//        return UserDefaults.standard.bool(forKey: "languageCodeEdited")
-//    }
-//}
-//
-//public var languageCodeApp: String {
-//    set {
-//        UserDefaults.standard.set(newValue, forKey: "languageCodeApp")
-//    }
-//    get {
-//        return UserDefaults.standard.string(forKey: "languageCodeApp") ?? LanguageCodeType.eng.rawValue
-//    }
-//}
-//
-//// MARK: METHODS TO WORK
-//public class LangStr {
-//
-//    public class func langStr(_ text: String) -> String {
-//        //        return NSLocalizedString(text, comment: text) // Old method to change language texts
-//        return  setLanguage(key: text)
-//    }
-//}
+//import Foundation
+//import UIKit
 //
 //
-//public func getLocaleLanguage() -> String {
-//    return Locale.preferredLanguages[0]
-//}
+//let languageManager = LanguageManager()
 //
-//public func setLanguage(key: String) -> String {
-//    return bundleLanguage.localizedString(forKey: key, value: nil, table: nil)
-//}
 //
-//public func setLanguagelUppercase(label: UILabel, keyText: String) {
-//    label.text = setLanguage(key: keyText).uppercased()
-//}
+//class LanguageManager {
 //
-//public func setLanguageExtra(label: UILabel, keyText: String, extraText: String = "") {
-//    label.text = setLanguage(key: keyText) + extraText
-//}
+//    init() {
 //
-//// MARK: BUNDLE
-//
-//public func setBundleLanguage() {
-//
-//    print("LanguageCodeEdited: \(languageCodeEdited)")
-//    if !languageCodeEdited {
-//        languageCodeApp = getLocaleLanguage()
-//        print("LanguageCodeApp-Device: \(languageCodeApp)")
-//        controlCodes()
-//        print("LanguageCodeApp-Device-Controled: \(languageCodeApp)")
-//        controlInitialLanguage()
-//        print("LanguageCodeApp-Initial: \(languageCodeApp)")
 //    }
 //
-//    let path = Bundle.main.path(forResource: languageCodeApp, ofType: "lproj")
-//    bundleLanguage = Bundle(path: path!)!
-//}
+//    // MARK: LANGUAGES CODES
+//    enum LanguageCodeType: String {
+//        case cat = "ca" //optional: "ca-ES"
+//        case cas = "es"
+//        case eng = "en"
+//    }
 //
-//func controlInitialLanguage() {
-//    var languageFounded = false
-//    if !languageCodeEdited {
-//        for language in appLanguages {
-//            if language.rawValue == languageCodeApp {
-//                languageFounded = true
-//                break
+//    // MARK: VARIABLES
+//    var bundleLanguage = Bundle()
+//    let appLanguages = [LanguageCodeType.cat, LanguageCodeType.cas, LanguageCodeType.eng]
+//
+//    // MARK: DEFAULTS REFERENCES
+//
+//    var languageCodeEdited: Bool {
+//        set {
+//            UserDefaults.standard.set(newValue, forKey: "languageCodeEdited")
+//        }
+//        get {
+//            return UserDefaults.standard.bool(forKey: "languageCodeEdited")
+//        }
+//    }
+//
+//    var languageCodeApp: String {
+//        set {
+//            UserDefaults.standard.set(newValue, forKey: "languageManager.languageCodeApp")
+//        }
+//        get {
+//            return UserDefaults.standard.string(forKey: "languageManager.languageCodeApp") ?? LanguageCodeType.eng.rawValue
+//        }
+//    }
+//
+//    // MARK: METHODS TO WORK
+//    func getLocaleLanguage() -> String {
+//        return Locale.preferredLanguages[0]
+//    }
+//
+//    func getMultilingualText(key: String) -> String {
+//        return bundleLanguage.localizedString(forKey: key, value: nil, table: nil)
+//    }
+//
+//    func setLanguagelUppercase(label: UILabel, keyText: String) {
+//        label.text = languageManager.getMultilingualText(key: keyText).uppercased()
+//    }
+//
+//    func setLanguageExtra(label: UILabel, keyText: String, extraText: String = "") {
+//        label.text = languageManager.getMultilingualText(key: keyText) + extraText
+//    }
+//
+//    // MARK: BUNDLE
+//
+//    func setBundleLanguage() {
+//
+//        print("LanguageCodeEdited: \(languageCodeEdited)")
+//        if !languageCodeEdited {
+//            languageManager.languageCodeApp = getLocaleLanguage()
+//            print("languageManager.languageCodeApp-Device: \(languageManager.languageCodeApp)")
+//            controlCodes()
+//            print("languageManager.languageCodeApp-Device-Controled: \(languageManager.languageCodeApp)")
+//            controlInitialLanguage()
+//            print("languageManager.languageCodeApp-Initial: \(languageManager.languageCodeApp)")
+//        }
+//
+//        let path = Bundle.main.path(forResource: languageManager.languageCodeApp, ofType: "lproj")
+//        bundleLanguage = Bundle(path: path!)!
+//    }
+//
+//    func controlInitialLanguage() {
+//        var languageFounded = false
+//        if !languageCodeEdited {
+//            for language in appLanguages {
+//                if language.rawValue == languageManager.languageCodeApp {
+//                    languageFounded = true
+//                    break
+//                }
+//            }
+//
+//            if !languageFounded {
+//                languageManager.languageCodeApp = LanguageCodeType.eng.rawValue
 //            }
 //        }
-//
-//        if !languageFounded {
-//            languageCodeApp = LanguageCodeType.eng.rawValue
-//        }
 //    }
-//}
 //
-//func controlCodes() {
+//    func controlCodes() {
 //
-//    switch languageCodeApp {
-//        //    case "ca":
-//    //        languageCodeApp = "ca-ES"
-//    case "ca-ES":
-//        languageCodeApp = "ca"
-//    case "en-US":
-//        languageCodeApp = "en"
-//    case "es-ES":
-//        languageCodeApp = "es"
-//    default:
-//        break
-//    }
-//}
-//
-//public func getLanguageText(language: String) -> String {
-//
-//    switch language {
-//    case LanguageCodeType.eng.rawValue:
-//        return setLanguage(key: "lang_en")
-//    case LanguageCodeType.cat.rawValue:
-//        return setLanguage(key: "lang_ca")
-//    case LanguageCodeType.cas.rawValue:
-//        return setLanguage(key: "lang_es")
-//    default:
-//        return ""
-//    }
-//}
-//
-//public func getCurrentLanguageText() -> String {
-//    return getLanguageText(language: languageCodeApp)
-//}
-//
-//public func getLanguageId() -> Int {
-//
-//    // id's language according to name and description order in array product data from server.
-//    switch languageCodeApp {
-//    case LanguageCodeType.cat.rawValue:
-//        return 0
-//    case LanguageCodeType.cas.rawValue:
-//        return 1
-//    case LanguageCodeType.eng.rawValue:
-//        return 2
-//    default:
-//        return 2
-//    }
-//}
-//
-//public func updateLanguageFromWSData(languageData: String) {
-//    for language in appLanguages {
-//        if language.rawValue == languageData {
-//            languageCodeEdited = true
-//            languageCodeApp = languageData
-//            setBundleLanguage()
+//        switch languageManager.languageCodeApp {
+//            //    case "ca":
+//        //        languageManager.languageCodeApp = "ca-ES"
+//        case "ca-ES":
+//            languageManager.languageCodeApp = "ca"
+//        case "en-GB":
+//            languageManager.languageCodeApp = "en"
+//        case "es-ES":
+//            languageManager.languageCodeApp = "es"
+//        default:
 //            break
 //        }
 //    }
+//
+//    func getLanguageText(language: String) -> String {
+//
+//        switch language {
+//        case LanguageCodeType.eng.rawValue:
+//            return languageManager.getMultilingualText(key: "lang_en")
+//        case LanguageCodeType.cat.rawValue:
+//            return languageManager.getMultilingualText(key: "lang_ca")
+//        case LanguageCodeType.cas.rawValue:
+//            return languageManager.getMultilingualText(key: "lang_es")
+//        default:
+//            return ""
+//        }
+//    }
+//
+//    func getLanguageTextShort(language: String) -> String {
+//
+//        switch language {
+//        case LanguageCodeType.eng.rawValue:
+//            return "EN"
+//        case LanguageCodeType.cat.rawValue:
+//            return "CAT"
+//        case LanguageCodeType.cas.rawValue:
+//            return "ES"
+//        default:
+//            return ""
+//        }
+//    }
+//
+//    func setLanguageCodeFromText(language: String) {
+//
+//        switch language {
+//        case languageManager.getMultilingualText(key: "lang_en"):
+//            languageManager.languageCodeApp = LanguageCodeType.eng.rawValue
+//            return
+//        case languageManager.getMultilingualText(key: "lang_ca"):
+//            languageManager.languageCodeApp = LanguageCodeType.cat.rawValue
+//            return
+//        case languageManager.getMultilingualText(key: "lang_es"):
+//            languageManager.languageCodeApp = LanguageCodeType.cas.rawValue
+//            return
+//        default:
+//            languageManager.languageCodeApp = LanguageCodeType.cas.rawValue
+//            return
+//        }
+//    }
+//
+//
+//    func getCurrentLanguageText() -> String {
+//        return getLanguageText(language: languageManager.languageCodeApp)
+//    }
+//
+//    func getCurrentLanguageTextShort() -> String {
+//        return getLanguageTextShort(language: languageManager.languageCodeApp)
+//    }
+//
+//    func getLanguageId() -> Int {
+//
+//        // id's language according to name and description order in array product data from server.
+//        switch languageManager.languageCodeApp {
+//        case LanguageCodeType.cat.rawValue:
+//            return 0
+//        case LanguageCodeType.cas.rawValue:
+//            return 1
+//        case LanguageCodeType.eng.rawValue:
+//            return 2
+//        default:
+//            return 2
+//        }
+//    }
+//
+//    func updateLanguageFromWSData(languageData: String) {
+//        for language in appLanguages {
+//            if language.rawValue == languageData {
+//                languageCodeEdited = true
+//                languageManager.languageCodeApp = languageData
+//                setBundleLanguage()
+//                break
+//            }
+//        }
+//    }
+//
+//
 //}
+//
 //
