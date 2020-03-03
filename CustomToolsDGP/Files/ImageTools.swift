c263fd7e9245c7b196b85bc29afe61724fd3171a
David Galán <david.galan@studiogenesis.es>
2020-03-03 17:26:07 +0100

    Updated ImageTools code.

--- CustomToolsDGP/Files/ImageTools.swift
+++ CustomToolsDGP/Files/ImageTools.swift
@@ -115,3 +115,22 @@ public class ImageTools {
 }
  
 
+extension UIView {
+
+    func asImage() -> UIImage? {
+        if #available(iOS 10.0, *) {
+            let renderer = UIGraphicsImageRenderer(bounds: bounds)
+            return renderer.image { rendererContext in
+                layer.render(in: rendererContext.cgContext)
+            }
+        } else {
+            UIGraphicsBeginImageContextWithOptions(self.bounds.size, self.isOpaque, 0.0)
+            defer { UIGraphicsEndImageContext() }
+            guard let currentContext = UIGraphicsGetCurrentContext() else {
+                return nil
+            }
+            self.layer.render(in: currentContext)
+            return UIGraphicsGetImageFromCurrentImageContext()
+        }
+    }
+}
