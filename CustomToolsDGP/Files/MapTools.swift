e4da32e2953bb579bd77a6bbd602214293f4ce9d
David Galán <david.galan@studiogenesis.es>
2020-03-10 17:15:25 +0100

    Updated MapTools code.

--- CustomToolsDGP/Files/MapTools.swift
+++ CustomToolsDGP/Files/MapTools.swift
@@ -24,6 +24,13 @@ public class MapTools {
         case walking
     }
     
+    public func goToPositionInMap(latitude: Double, longitude: Double, locationDregrees: Double = 0.001, setRegionAnimated: Bool = true) {
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        self.mapView?.setRegion(region, animated: setRegionAnimated)
+    }
+    
     public func setAnnotationInMap(latitude: Double, longitude: Double, locationDregrees: Double = 0.001, customAnnotation: Bool, title: String = "", subtitle: String = "", imageName: String = "", setRegionAnimated: Bool = true, withEyeCoordinate: Bool = false, eyeCoordinateValue: Double = 0.001, eyeAltitudeValue: Double = 200, setCameraAnimated: Bool = true, setCenterValue: Bool = false, setCenterAnimated: Bool = true, disableCenterCamera: Bool = false) {
         
         let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: locationDregrees, longitudeDelta: locationDregrees)
