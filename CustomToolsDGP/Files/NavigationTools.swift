79c7da6942b353b5fa114921354a5ed1fdc90291
David Galán <david.galan@studiogenesis.es>
2020-06-23 16:35:45 +0200

    Updated NavigationTools code behavior.

--- CustomToolsDGP/Files/NavigationTools.swift
+++ CustomToolsDGP/Files/NavigationTools.swift
@@ -15,39 +15,54 @@ public func getNewViewController(storyBoardName: String, viewIdentifier: String)
 extension UIViewController {
     
     public func disableSwipeBackGesture(enabled: Bool) {
-        self.navigationController?.interactivePopGestureRecognizer?.isEnabled = enabled
+        DispatchQueue.main.async {
+            self.navigationController?.interactivePopGestureRecognizer?.isEnabled = enabled
+        }
     }
     
     public func goToPreviousView() {
-        self.navigationController?.popViewController(animated: true)
+        DispatchQueue.main.async {
+            self.navigationController?.popViewController(animated: true)
+        }
     }
     
     public func goBackToRootView() {
-        self.navigationController?.popToRootViewController(animated: true)
+        DispatchQueue.main.async {
+            self.navigationController?.popToRootViewController(animated: true)
+        }
     }
     
     public func openPopupView(viewController: UIViewController, alphaBlackComponent: CGFloat?) {
         if alphaBlackComponent != nil {
             viewController.view.backgroundColor = UIColor.black.withAlphaComponent(alphaBlackComponent!)
         }
-        self.addChild(viewController)
-        self.view.addSubview(viewController.view)
+        
+        DispatchQueue.main.async {
+            self.addChild(viewController)
+            self.view.addSubview(viewController.view)
+        }
     }
     
     public func openPopupViewInFrontNavigation(viewController: UIViewController, alphaBlackComponent: CGFloat?) {
         if alphaBlackComponent != nil {
             viewController.view.backgroundColor = UIColor.black.withAlphaComponent(alphaBlackComponent!)
         }
-        self.navigationController?.addChild(viewController)
-        self.navigationController?.view.addSubview(viewController.view)
+        
+        DispatchQueue.main.async {
+            self.navigationController?.addChild(viewController)
+            self.navigationController?.view.addSubview(viewController.view)
+        }
     }
     
     public func openPopupViewInFrontTabBar(viewController: UIViewController, alphaBlackComponent: CGFloat?) {
         if alphaBlackComponent != nil {
             viewController.view.backgroundColor = UIColor.black.withAlphaComponent(alphaBlackComponent!)
         }
-        self.tabBarController?.addChild(viewController)
-        self.tabBarController?.view.addSubview(viewController.view)
+        
+        DispatchQueue.main.async {
+            self.tabBarController?.addChild(viewController)
+            self.tabBarController?.view.addSubview(viewController.view)
+        }
     }
     
     public func openPopupView(storyBoardName: String, viewIdentifier: String, alphaBlackComponent: CGFloat?) {
@@ -59,19 +74,24 @@ extension UIViewController {
             vc.view.backgroundColor = UIColor.black.withAlphaComponent(alphaBlackComponent!)
         }
         
-        self.addChild(vc)
-        self.view.addSubview(vc.view)
+        DispatchQueue.main.async {
+            self.addChild(vc)
+            self.view.addSubview(vc.view)
+        }
     }
     
     public func openVieWithConstraintsStandard(customView: UIViewController) {
         
-        self.openPopupView(viewController: customView, alphaBlackComponent: 0.0)
+        DispatchQueue.main.async {
+            self.openPopupView(viewController: customView, alphaBlackComponent: 0.0)
+            
+            customView.view.translatesAutoresizingMaskIntoConstraints = false
+            customView.view.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0).isActive = true
+            customView.view.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0).isActive = true
+            customView.view.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0).isActive = true
+            customView.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0).isActive = true
+        }
         
-        customView.view.translatesAutoresizingMaskIntoConstraints = false
-        customView.view.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0).isActive = true
-        customView.view.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0).isActive = true
-        customView.view.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0).isActive = true
-        customView.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0).isActive = true
     }
     
     public func closePopupView() {
