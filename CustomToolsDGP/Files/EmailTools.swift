eeefcfd280f1eb20be2a6fb80c35c7ed06b78ef9
David Galán <david.galan@studiogenesis.es>
2020-07-02 15:59:04 +0200

    Restored EmailTools code.

--- CustomToolsDGP/Files/EmailTools.swift
+++ CustomToolsDGP/Files/EmailTools.swift
@@ -11,26 +11,15 @@ extension String {
     public func isValidEmail() -> Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
         let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
-        return emailTest.evaluate(with: self.checkLastCharacterOfEmail())
+        return emailTest.evaluate(with: self)
     }
     
     public func openEmail() {
         if self.isValidEmail() {
-            if let url = URL(string: "mailto:\(self.checkLastCharacterOfEmail())") {
+            if let url = URL(string: "mailto:\(self)") {
                 UIApplication.shared.open(url)
             }
         }
-    }
-    
-    private func checkLastCharacterOfEmail() -> String {
-        
-        var emailToCheck: String = self
-        
-        if emailToCheck.last == " " {
-            emailToCheck = String(emailToCheck.dropLast())
-        }
-        
-        return emailToCheck
     }
     
 }
