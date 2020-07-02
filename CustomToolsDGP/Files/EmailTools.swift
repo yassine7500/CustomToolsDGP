1b742c6f7dd41928ccdceca76e52f0315bc8dadf
David Galán <david.galan@studiogenesis.es>
2020-07-02 15:57:42 +0200

    Updated EmailTools behavior of methods.

--- CustomToolsDGP/Files/EmailTools.swift
+++ CustomToolsDGP/Files/EmailTools.swift
@@ -11,15 +11,26 @@ extension String {
     public func isValidEmail() -> Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
         let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
-        return emailTest.evaluate(with: self)
+        return emailTest.evaluate(with: self.checkLastCharacterOfEmail())
     }
     
     public func openEmail() {
         if self.isValidEmail() {
-            if let url = URL(string: "mailto:\(self)") {
+            if let url = URL(string: "mailto:\(self.checkLastCharacterOfEmail())") {
                 UIApplication.shared.open(url)
             }
         }
+    }
+    
+    private func checkLastCharacterOfEmail() -> String {
+        
+        var emailToCheck: String = self
+        
+        if emailToCheck.last == " " {
+            emailToCheck = String(emailToCheck.dropLast())
+        }
+        
+        return emailToCheck
     }
     
 }
