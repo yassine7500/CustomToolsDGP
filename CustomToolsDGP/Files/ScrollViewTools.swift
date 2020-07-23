6603ae32086e407d4731cdf491a229bd1ef6c90d
David Galán <david.galan@studiogenesis.es>
2020-07-23 10:57:46 +0200

    Created ScrollViewTools file.

---
+++ CustomToolsDGP/Files/ScrollViewTools.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollViewTools.swift
+//  CustomToolsDGP
+//
+//  Created by David SG on 23/07/2020.
+//  Copyright © 2020 David Galán. All rights reserved.
+//
+
+extension UIScrollView {
+
+    func scrollTo(horizontalPage: Int? = 0, verticalPage: Int? = 0, animated: Bool? = true) {
+        var frame: CGRect = self.frame
+        frame.origin.x = frame.size.width * CGFloat(horizontalPage ?? 0)
+        frame.origin.y = frame.size.width * CGFloat(verticalPage ?? 0)
+        self.scrollRectToVisible(frame, animated: animated ?? true)
+    }
+
+}
