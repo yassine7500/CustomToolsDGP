2c1d09fab39a84077fbbde6b1f3ed0d7e14f02bb
David Galán <david.galan@studiogenesis.es>
2020-09-04 11:32:53 +0200

    Created FileDownloaderCustom code.

---
+++ CustomToolsDGP/Files/FileDownloaderCustom.swift
@@ -0,0 +1,48 @@
+//
+//  FileDownloaderCustom.swift
+//  CustomToolsDGP
+//
+//  Created by David SG on 04/09/2020.
+//  Copyright © 2020 David Galán. All rights reserved.
+//
+
+class FileDownloaderCustom {
+
+    static func loadFileAsync(fileName: String, url: URL, completion: @escaping (String?, Error?) -> Void) {
+        
+        let documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let destinationUrl = documentsUrl.appendingPathComponent(url.lastPathComponent)
+
+        if FileManager().fileExists(atPath: destinationUrl.path) {
+            print("File already exists [\(destinationUrl.path)]")
+            completion(destinationUrl.path, nil)
+        } else {
+            
+            let session = URLSession(configuration: URLSessionConfiguration.default, delegate: nil, delegateQueue: nil)
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            
+            let task = session.dataTask(with: request, completionHandler: {
+                data, response, error in
+                if error == nil {
+                    if let response = response as? HTTPURLResponse {
+                        if response.statusCode == 200 {
+                            if let data = data {
+                                if let _ = try? data.write(to: destinationUrl, options: Data.WritingOptions.atomic) {
+                                    completion(destinationUrl.path, error)
+                                } else {
+                                    completion(destinationUrl.path, error)
+                                }
+                            } else {
+                                completion(destinationUrl.path, error)
+                            }
+                        }
+                    }
+                } else {
+                    completion(destinationUrl.path, error)
+                }
+            })
+            task.resume()
+        }
+    }
+}
